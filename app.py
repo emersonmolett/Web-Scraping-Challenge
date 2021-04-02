2550a6586c5e1a346c5ee410d1f3b8339bac5cbf
Emerson Molett <emerson.molett@gmail.com>
2021-04-02 05:07:11 -0500

    made tweaks to mars facts code in order to clean up the data table

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -26,9 +26,12 @@ def scrape():
 
     # Run the scrape function
     mars_data = scrape_mars.scrape_info()
+    
+    # call the scrape() function you imported above 
+    scraped_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mars_data.update({}, scraped_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
