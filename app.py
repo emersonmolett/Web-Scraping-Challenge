c5ae50b7d929067770063031446374dd2c3e20ac
Emerson Molett <emerson.molett@gmail.com>
2021-04-05 18:27:00 -0500

    updated how the database was listed,     mars_data = mongo.db.mars.find_one(). was able to get webpage to load data. need to work on loading the featured immage and the hemis

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.mars_data.find_one()
+    mars_data = mongo.db.mars.find_one()
 
     # Return template and data
     return render_template("index.html", mars=mars_data)
